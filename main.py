178307cdf5cf659132347c861d4809570582938d
leafar1087 <leafar1087@gmail.com>
2025-11-04 22:20:03 +0100

    Modulo 2.7: Separada la logica de analisis al modulo 'analyzer.py'

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-# Módulo 2.6. FUNCIONES (con 'return' y 'annotations')
+# Módulo 2.7: Modulos y paquetes
+# main.py - nuestro script principal
 
-print("--- Iniciando Analizador SIEM v0.4 (Buenas practicas) ---")
+# --- IMPORTACIONES ---
+# Importamos la funcion obtener_severidad del modulo analyzer.py
+from analyzer import obtener_severidad
+
+print("--- Iniciando Analizador SIEM v0.5 (Modular) ---")
 
 
 # --- DEFINICION DE DATOS ---
@@ -16,39 +21,20 @@ log_batch = [
 ]
 
 # --- DEFINICION DE FUNCIONES ---
+#   Las funciones que definimos en analyzer.py ya no son necesarias aqui
 
-# Introducimos 'Annotations' (Type Hinting)
-# (log_a_procesar: str) -> str significa que el parametro log_a_procesar debe ser un string y que la funcion devuelve un string
-
-def obtener_severidad(log_a_procesar: str) -> str:
-
-    """
-    Analiza un unico string de log y DEVUELVE su severidad
-    """
-
-    print(f"Procesando log: {log_a_procesar}")
-    
-    if "ERROR" in log_a_procesar or "Fallo" in log_a_procesar:
-        return "ALERTA" # EN LUGAR DE IMPRIMIR, devolvemos la cadena "ALERTA"
-    elif "WARN" in log_a_procesar or "Advertencia" in log_a_procesar:
-        return "AVISO" # EN LUGAR DE IMPRIMIR, devolvemos la cadena "AVISO"
-    else:
-        return "INFORMACION" # EN LUGAR DE IMPRIMIR, devolvemos la cadena "INFORMACION"
 
 # --- EJECUCION PRINCIPAL ---
 # (EL 'CEREBRO' DEL PROGRAMA)
 
-print("--- Comenzando analisis en lotes ---")
+print("\n--- COMENZANDO ANALISIS EN LOTES ---")
 
 for log_individual in log_batch:
-    # 1. Llamamos a la funcion y capturamos su resultado en una variable
+    
     severidad = obtener_severidad(log_individual)
-
-    # 2. Ahora el bucle principal decide que hacer con el resultado
     print(f"Log: {log_individual} | Severidad: {severidad}")
 
-    # Podemos tomar decisiones basadas en el resutado
     if severidad == "ALERTA":
-        print(f"    [ALERTA] Se ha detectado un evento critico en seguridad. Enviando email al administrador")
+        print(f"    [ALERTA] ACCION REQUERIDA: Enviando email al administrador")
 
 print("--- ANALISIS DE LOGS FINALIZADO ---")
