b522e12dee0c820105a841b6701b0bef82a903fe
leafar1087 <leafar1087@gmail.com>
2025-11-04 21:48:10 +0100

    Modulo 2: Implementando funciones v1'

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 # Módulo 2.5: Tipos de Datos avanzados (listas, tuplas, diccionarios)
 # Modulo 2.4: Control de flujo (bucles)
 
-print(f"--- Iniciando Analizador SIEM v0.2 (Procesamiento por Lotes) ---")
-
-# 1. Una lista de logs (tipo 'list')
-# en lugar de un solo string, ahora tenemos una lista de strings
+# --- DEFINICION DE DATOS ---
+# (NUESTROS LOGS DE PRUEBA)
 
 log_batch = [
     "INFO: 2025-11-04T20:01:00 - 192.168.1.1 - Conexión exitosa",
@@ -15,34 +13,35 @@ log_batch = [
     "ERROR: 2025-11-04T20:05:00 - 10.0.0.5 - Fallo de autenticación"
 ]
 
-# 2. Accediendo a elementos de la lista (por indice)
-# Python empieza a contar desde 0, por lo que el primer elemento es el 0, el segundo es el 1, etc.
+# --- DEFINICION DE FUNCIONES ---
 
-print(f"Primer log: {log_batch[0]}")
-print(f"Segundo log: {log_batch[1]}")
-print(f"Ultimo log: {log_batch[-1]}")
+# Implememntacion  de la funcion analizar_log(log_a_procesar)
+# 'log_a_procesar' es un PARAMETRO de la funcion
 
-# Tambien podemos saber cuantos logs tenemos en la lista
-print(f"Total de logs a analizar: {len(log_batch)}")
+def analizar_log(log_a_procesar):
 
-# 3. Iterando sobre la lista (usando bucles)
-# Un bucle es un bloque de codigo que se ejecuta repetidamente hasta que se cumple una condicion
-# Usamos la palabra clave 'for' para iterar sobre la lista
-# La variable 'log' va tomando sucesivamente cada elemento de la lista
+    """
+    Este es un 'docstring'
+    Analiza un unico string de log e imprime su severidad
+    """
 
-print("--- Analizando logs ---")
-for log in log_batch:
-    print(f"Procesando log: {log}")
-
-    # Reutilizamos nuestra logica if/elif/else de la leccion anterior
-    # Pero ahora lo aplicamos a la variable 'log' que contiene cada log de la lista
-
-    if "ERROR" in log or "Fallo" in log:
+    print(f"Procesando log: {log_a_procesar}")
+    
+    if "ERROR" in log_a_procesar or "Fallo" in log_a_procesar:
         print(f"    [ALERTA] Se ha detectado un evento critico en seguridad")
-    elif "WARN" in log or "Advertencia" in log:
+    elif "WARN" in log_a_procesar or "Advertencia" in log_a_procesar:
         print(f"    [AVISO] Evento de advertencia detectado")
     
     # No necesitamos else paa los INFO, ya que no son eventos críticos
 
-print("--- Fin del analisis de logs ---")
+# --- EJECUCION PRINCIPAL ---
+# (EL 'CEREBRO' DEL PROGRAMA)
 
+print("--- Iniciando Analizador SIEM v0.3 (Procesamiento por Lotes) ---")
+
+# Iteramos sobre cada log en la lista de logs de prueba
+for log in log_batch:
+    # Llamamos a la funcion analizar_log con el log actual
+    analizar_log(log)
+
+print("--- ANALISIS DE LOGS FINALIZADO ---")
